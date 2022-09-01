50f3565eafd8cb75fa51d476d7a1bad7b0d9f3ac
GPC <anthonytguilherme@hotmail.com>
2022-09-01 17:07:42 -0300

    create the header and stylish with SASS

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './styles/global.scss';
-import styles from './app.module.css';
+import styles from './app.module.scss';
+
+import logoSvg from './assets/logo.svg'
 
 function App() {
   return (
     <header className={styles.header}>
-      <h1>Hello Friend!</h1>
+      <img className="logo" src={logoSvg} alt="AfroToDo" />
     </header>
   )
   
