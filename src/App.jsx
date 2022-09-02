735efdee7b0594de109fa8223172c2f0e8fd338f
GPC <anthonytguilherme@hotmail.com>
2022-09-02 15:27:07 -0300

    create the cards components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import './styles/global.scss';
 import styles from './App.module.scss'
 
-import {Header} from './components/Header'
-import {Plus} from 'phosphor-react'
+import { Header } from './components/Header'
+import { Plus, Trash } from 'phosphor-react'
 
 function App() {
   return (
@@ -13,6 +13,50 @@ function App() {
           <input type="text" placeholder="Insira uma nova atividade"></input>
           <button><Plus /></button>
         </form>
+
+        <h3 className={styles.status}>Tarefas concluídas <span> 3 de 6</span></h3>
+
+        <ul className={styles.taskList}>
+          <li>
+            <input type="checkbox" />
+
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut expedita
+              aliquid temporibus magnam voluptates adipisci officia, quis neque minus?
+              Laboriosam dolor excepturi eveniet iste velit amet, commodi consequatur a magni!
+            </p>
+
+            <button>
+              <Trash />
+            </button>
+
+          </li>
+          <li>
+            <input type="checkbox" />
+
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut expedita
+              aliquid temporibus magnam voluptates adipisci officia, quis neque minus?
+              Laboriosam dolor excepturi eveniet iste velit amet, commodi consequatur a magni!
+            </p>
+
+            <button>
+              <Trash />
+            </button>
+          </li>
+          <li>
+            <input type="checkbox" />
+
+            <p>
+              Lorem ipsum dolor sit, amet
+            </p>
+
+            <button>
+              <Trash />
+            </button>
+
+          </li>
+        </ul>
       </main>
     </>
   )
